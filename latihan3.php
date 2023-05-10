d196df640c934f4da03e3488ce29a1d6492ac66f
Sitikhairohauliyamuslikha <123297331+Sitikhairohauliyamuslikha@users.noreply.github.com>
2023-05-10 08:28:44 +0700

    Add files via upload

---
+++ latihan3.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>percobaan php</title>
+</head>
+<body>
+    <?php printf("Helllo Word");?>
+</body>
+</html>
---
+++ latihan4.php
@@ -0,0 +1,7 @@
+<?php
+echo"<h2>Belajar PHP itu mudah!</h2>";
+echo("Hello Word1,br>");
+echo"Aku sedang belajar PHP!<br>";
+echo"ini","teks","yang","dibuat","terpisah";
+
+?>
